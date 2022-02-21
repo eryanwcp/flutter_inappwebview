7bbf06c891ab685d099dd5b718190b959a9968e9
eryan <eryanwcp@gmail.com>
2022-02-21 14:46:07 +0800

    合并（下载优化）

--- android/src/main/java/com/pichillilorenzo/flutter_inappwebview/MyCookieManager.java
+++ android/src/main/java/com/pichillilorenzo/flutter_inappwebview/MyCookieManager.java
@@ -160,12 +160,7 @@ public class MyCookieManager implements MethodChannel.MethodCallHandler {
     cookieValue += ";";
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-      cookieManager.setCookie(url, cookieValue, new ValueCallback<Boolean>() {
-        @Override
-        public void onReceiveValue(Boolean aBoolean) {
-          result.success(true);
-        }
-      });
+      cookieManager.setCookie(url, cookieValue, aBoolean -> result.success(true));
       cookieManager.flush();
     }
     else {
