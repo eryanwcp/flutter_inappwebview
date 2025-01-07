fbe466482fa8a586dbd28b1ec9f0c4c82db6c767
eryan <eryanwcp@gmail.com>
2025-01-07 15:55:34 +0800

    Android15.0 状态栏适配

--- flutter_inappwebview_android/android/src/main/java/com/pichillilorenzo/flutter_inappwebview_android/in_app_browser/InAppBrowserActivity.java
+++ flutter_inappwebview_android/android/src/main/java/com/pichillilorenzo/flutter_inappwebview_android/in_app_browser/InAppBrowserActivity.java
@@ -226,7 +226,8 @@ public class InAppBrowserActivity extends AppCompatActivity implements InAppBrow
       if (customSettings.toolbarTopFixedTitle != null && !customSettings.toolbarTopFixedTitle.isEmpty())
         actionBar.setTitle(customSettings.toolbarTopFixedTitle);
 
-      hideStatusBar(this);
+      CustomBackground.setDecorBackground(this);
+//      hideStatusBar(this);
     }
   }
 
